8c68aec3970b5fd2799d636f564cd767f038faf5
XZ <register.xizhou2@gmail.com>
2015-09-26 18:32:09 -0700

    added long polling for real time update.

--- VideoRetriever.php
+++ VideoRetriever.php
@@ -2,9 +2,16 @@
 	//require_once("pecl_http");
 	putenv('PATH=' . getenv('PATH') . PATH_SEPARATOR . '/usr/local/bin');
 	class VideoRetriever {
+		public function __construct() {
+			$sid = session_id();
+			$this->progressFile = "Downloads/".$sid.".txt";
+			fopen($this->progressFile, "w") or die("Can not create session file ".$sid.".txt");
+			fclose($this->progressFile);
+		}
 		public function convert() {
 			$host = $_SERVER["HTTP_HOST"];
 			$url = $_REQUEST["youtube_url"];
+
 			//$vid = "UybVeuP-Lxo";
 			//$url = http_build_url(); 	// default is current page
 			// Or use this:
@@ -37,24 +44,37 @@
 			// Check if the converted file already exists
 			if(file_exists("Downloads/".$vid.".mp3")) {
 				error_log("Requestd file in the cache: id: " . $vid);
+				$meta = $this->getVideoMeta($vid);
 				$arr = array(
 					"return" => "0",
 					"output" => "File in the cache",
-					"vid"	 => $vid
+					"vid"	 => $vid,
+					"meta"	 => $meta
 				);
 				echo json_encode($arr);
 				exit;
 			} else {
+				// update the file in real time
+				//header("Content-type: text/plain");
+				// prevent output buffering
+				$this->disable_ob();
 				error_log("Downloading youtube from youtube.com");
-				exec("youtube-dl -x ".$url." --audio-format mp3 -o 'Downloads/%(id)s.%(ext)s'", $output, $ret);
+				//exec("youtube-dl -x ".$url." --audio-format mp3 --write-info-json -o 'Downloads/%(id)s.%(ext)s'", $output, $ret);
+				//system("youtube-dl -x ".$url." --audio-format mp3 --write-info-json -o 'Downloads/%(id)s.%(ext)s'");
+				$ret = $this->convertVideoRealtimeOutput("youtube-dl -x ".$url." --audio-format mp3 --write-info-json -o 'Downloads/%(id)s.%(ext)s'", $url);
 				error_log("Converstion has completed");
 
+				// Get meta data from the the meta file
+				
+				$meta = $this->getVideoMeta($vid);
 				$arr = array(
-					"return" => $ret,
-					"output" => $output,
-					"vid"	 => $vid
+					"return" 	=> $ret,
+					"output"	=> file_get_contents($this->progressFile),
+					"vid" 		=> $vid,
+					"meta" 		=> $meta
 					);
 				echo json_encode($arr);
+				
 			}	
 		}
 
@@ -62,13 +82,157 @@
 			// Push files to client 
 			if(isset($_REQUEST["vid"])) {
 				$filename = $_REQUEST["vid"].".mp3";
+
+				// get the video title and substitute filename with the title
+				$meta_str = file_get_contents("Downloads/".$_REQUEST["vid"].".info.json");
+				$meta_json = json_decode($meta_str, true);
+
 				header("Content-type:application/mp3");
-				header("Content-Disposition:attachement;filename=".$filename);
+				header("Content-Disposition:attachement;filename=".$meta_json["title"].".mp3");
 				readfile("Downloads/".$filename);
 				flush();
 				error_log("File: ".$filename." has been pushed to user.");
 			} else {
 				error_log("Fail to push file ".$filename." to user.");
+			}
+		}
+
+		public function getConvertStatus() {
+			// server side long poll to reture conversion status to front end.
+			$startTime = time();
+			$lastChangeTime = isset($_REQUEST["timestamp"]) ? $_REQUEST["timestamp"] : 0;
+			clearstatcache();
+			if(file_exists($this->progressFile)) {
+				$changeTime = filemtime($this->progressFile);
+			} else {
+				$changeTime = 0;
+			}
+			
+			error_log("lastChangeTime: " . $lastChangeTime . " changeTime: " . $changeTime);
+			while($changeTime <= $lastChangeTime && time() - $startTime < 20) {
+				// no message so just spin here
+				usleep(2000000);
+				clearstatcache();
+				if(file_exists($this->progressFile)) {
+					$changeTime = filemtime($this->progressFile);
+				} else {
+					$changeTime = 0;
+				}
+//				error_log("file change time: ". $changeTime . " request timestamp " . $lastChangeTime);
+			}
+			if($changeTime == 0) {
+				// No status since status file does not exist
+				$ret = array(
+					"timestamp" => $changeTime,
+					"content"	=> "0");
+			} else {
+				$ret = array(
+					"timestamp" => $changeTime,
+					"content"	=> file_get_contents($this->progressFile));
+			}	
+			error_log("Return to poll: " . $ret["content"] . "timestamp: " . $ret["timestamp"] . 
+					"lastChangeTime " . $lastChangeTime);
+			echo json_encode($ret);
+			flush();
+
+			// delete the file if completed
+			if($ret["content"] == "4") {
+				error_log("Delete status file. " . $this->progressFile);
+				unlink($this->progressFile);
+			}
+		}
+
+		private function getVideoMeta($vid) {
+			// purpose: read meta data in the json meta file and return them 
+			// as an associative array
+				$meta_str = file_get_contents("Downloads/".$vid.".info.json");
+				$meta_json = json_decode($meta_str, true);
+				$meta = array( 
+					"title" => $meta_json["title"],
+					"duration" => $meta_json["duration"]
+					);
+
+				return $meta;
+		}
+
+		private function disable_ob() {
+		    // Turn off output buffering
+		    ini_set('output_buffering', 'off');
+		    // Turn off PHP output compression
+		    ini_set('zlib.output_compression', false);
+		    // Implicitly flush the buffer(s)
+		    ini_set('implicit_flush', true);
+		    ob_implicit_flush(true);
+		    // Clear, and turn off output buffering
+		    while (ob_get_level() > 0) {
+		        // Get the curent level
+		        $level = ob_get_level();
+		        // End the buffering
+		        ob_end_clean();
+		        // If the current level has not changed, abort
+		        if (ob_get_level() == $level) break;
+		    }
+		    // Disable apache output buffering/compression
+		    if (function_exists('apache_setenv')) {
+		        apache_setenv('no-gzip', '1');
+		        apache_setenv('dont-vary', '1');
+		    }
+		}
+
+		private function convertVideoRealtimeOutput ($cmd, $url) {
+			$descriptorspec = array(
+			   0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+			   1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+			   2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+			);
+			$pipes = array();
+			$process = proc_open($cmd, $descriptorspec, $pipes);
+			$status = 0;
+			$sid = session_id();
+						
+			if (is_resource($process)) {
+			    while (!feof($pipes[1])) {
+			    	// get the output from youtube-dl output, parse and write to pregress file
+
+			    	$s = fgets($pipes[1]);
+			    	if(preg_match("@^\[youtube\]@", $s) && $status == 0) {
+			    		// downloading meta data
+			    		// truncate the status files in case there is residues.
+			    		$file = fopen($this->progressFile, "w+");
+			    		if($file) {
+			    			ftruncate($file, 0);
+			    		}
+			    		error_log("Status 1. Session: " . $sid);
+			    		file_put_contents($this->progressFile, "1");
+			    		$status = 1;
+			    	} else if(preg_match("@^\[download\]@", $s) && $status == 1) {
+			    		// downloading video
+			    		error_log("Status 2. Session: " . $sid);
+			    		file_put_contents($this->progressFile, "2");
+			    		$status = 2;
+			    	} else if (preg_match("@^\[ffmpeg\]@", $s) && $status == 2) {
+			    		// converting video to audio
+			    		error_log("Status 3. Session: " . $sid);
+			    		file_put_contents($this->progressFile, "3");
+			    		$status = 3;
+			    	} else if (preg_match("@^Deleting@", $s) && $status == 3) {
+			    		// deleting video file, complete
+			    		error_log("Status 4. Session: " . $sid);
+			    		file_put_contents($this->progressFile, "4");
+			    		$status = 4;
+			    	}  else {
+			    		usleep(851000);
+			    	}
+//			    	clearstatcache();
+//			    	error_log("File change time: " . filemtime($this->progressFile) . " Status: " . $status);
+				}
+			}
+			proc_close($process);
+
+			if($status == 4) {
+				return 0;
+			} else {
+				return 1;
 			}
 		}
 	}
--- backend.php
+++ backend.php
@@ -2,9 +2,36 @@
 	// Retrieves Youtube video, convert it into mp3 and returns a retrieve url to the front page
 
 	require_once("VideoRetriever.php");
-	// TODO: Can you create VideoRetriever once?
-	$retriever = new VideoRetriever();
 
+	// start a user session and prevents others to access the session
+	ini_set("session.use_only_cookies", true);
+	session_start();
+
+/*
+	// add a token
+	$salt = "banana";
+	$tokenstr = strval(date('W')) . $salt;
+	$token = md5($tokenstr);
+
+	// check if token is present in the request
+	if(!isset($_REQUEST["token"]) || $_REQUEST["token"] != $token) {
+		error_log("User session is not correct and access has been rejected.");
+		exit;
+	}
+
+	$_SESSION["token"] = $token;
+	output_add_rewrite_var("token", $token);
+*/
+	// TODO: Can you create VideoRetriever once?
+	if(!isset($_SESSION["retriever"])) {
+		$retriever = new VideoRetriever();
+		$_SESSION["retriever"] = $retriever;
+	} else {
+		$retriever = $_SESSION["retriever"];
+	}
+
+	// prevent session block
+	session_write_close();
 
 	if(isset($_REQUEST["op"])) {
 		if($_REQUEST["op"] == "convert") {
@@ -12,6 +39,8 @@
 		} else if($_REQUEST["op"] == "download") {
 			error_log("pushing the video to client.");
 			return $retriever->get();
+		} else if($_REQUEST["op"] == "pollStatus") {
+			return $retriever->getConvertStatus();
 		}
 	} else {
 		return json_encode(
